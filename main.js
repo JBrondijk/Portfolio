e5e52895f2b6c2f53abc9934e2cf205b9b17cf8b
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-02-11 16:27:07 +0100

    Finished Section 3

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 import * as THREE from "./libs/three.js-r132/build/three.module.js";
 
-console.log("THREE", THREE);
-
 document.addEventListener("DOMContentLoaded",()=>{
 	const scene = new THREE.Scene();
 	
 	const geometry = new THREE.BoxGeometry(1,1,1);
 	const material = new THREE.MeshBasicMaterial({color: "#0000FF"});
 	const cube = new THREE.Mesh(geometry, material);
-	
-	scene.add(cube);
 	cube.position.set(0,0,-2);
 	cube.rotation.set(0,Math.PI/4,0);
-	
+	scene.add(cube);
+
 	const camera = new THREE.PerspectiveCamera();
 	camera.position.set(1,1,5);
 	
@@ -21,8 +18,8 @@ document.addEventListener("DOMContentLoaded",()=>{
 	renderer.render(scene,camera);
 	
 	const video = document.createElement("video");
-	navigator.mediaDevices.getUserMedia({video: true, audio:false, video:{facingMode:'environment'}}).then((stream)=>{
-		video.srcOject = stream;
+	navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
+		video.srcObject = stream;
 		video.play();
 	});
 	
