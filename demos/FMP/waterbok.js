dd378173bfc243ab4eb54c663e11c3718b0e56fb
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-03-02 16:52:03 +0100

    testing angles waterbok

--- demos/FMP/waterbok.js
+++ demos/FMP/waterbok.js
@@ -8,6 +8,9 @@ const cube = new THREE.Mesh(geometry, material);
 const camera = new THREE.PerspectiveCamera();
 const renderer = new THREE.WebGLRenderer({alpha:true});
 
+const angles = ["alphaBetaGamma","alphaGammaBeta","betaAlphaGamma", "betaGammaAlhpa","gammaAlphaBeta","gammaBetaAlpha"];
+const currentangle = 0;
+
 
 window.addEventListener("deviceorientation", handleOrientation, true);
 document.addEventListener("DOMContentLoaded",()=>{
@@ -40,6 +43,35 @@ function handleOrientation(event) {
   var alpha    = event.alpha*(Math.PI/180);
   var beta     = event.beta*(Math.PI/180);
   var gamma    = event.gamma*(Math.PI/180);
-  camera.rotation.set(alpha,beta,gamma);
+
+	  if (currentangle == 0){
+		camera.rotation.set(alpha,beta,gamma);
+	  }
+	  if (currentangle == 1){
+		camera.rotation.set(alpha,gamma,beta);
+	  }
+	  if (currentangle == 2){
+		camera.rotation.set(beta,alpha,gamma);
+	  }
+	  if (currentangle == 3){
+		camera.rotation.set(beta,gamma,alpha);
+	  }
+	  if (currentangle == 4){
+		camera.rotation.set(gamma,alpha,beta);
+	  }
+	  if (currentangle == 5){
+		camera.rotation.set(gamma,beta,alpha);
+	  }
+
   renderer.render(scene,camera);
+}
+
+toggle(){
+console.log("ToggleButton");
+	const element = document.getElementByID(button1);
+	currentangle = currentangle + 1;
+	if (currentangle > 5){
+		currentangle = 0;
+	}
+	button.textContent = angles[currentangle];
 }
