eea84fc245dacc497767584b1f9e8eea151fb2a2
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-03-02 15:53:50 +0100

    Fixed folder referneces to FMP demos Waterbok Test 2

--- demos/FMP/waterbok.js
+++ demos/FMP/waterbok.js
@@ -1,20 +1,23 @@
 import * as THREE from "../libs/three.js-r132/build/three.module.js";
 
+const scene = new THREE.Scene();
+	
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color: "#0000FF"});
+const cube = new THREE.Mesh(geometry, material);
+const camera = new THREE.PerspectiveCamera();
+const renderer = new THREE.WebGLRenderer({alpha:true});
+
+
 window.addEventListener("deviceorientation", handleOrientation, true);
 document.addEventListener("DOMContentLoaded",()=>{
-	const scene = new THREE.Scene();
 	
-	const geometry = new THREE.BoxGeometry(1,1,1);
-	const material = new THREE.MeshBasicMaterial({color: "#0000FF"});
-	const cube = new THREE.Mesh(geometry, material);
 	cube.position.set(0,0,-2);
 	cube.rotation.set(0,Math.PI/4,0);
 	scene.add(cube);
 
-	const camera = new THREE.PerspectiveCamera();
 	camera.position.set(1,1,5);
 	
-	const renderer = new THREE.WebGLRenderer({alpha:true});
 	renderer.setSize(500,500);
 	renderer.render(scene,camera);
 	
