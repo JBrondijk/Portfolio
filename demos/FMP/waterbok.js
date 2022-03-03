07f946b885bf8fc1e690c1da2e510a199e3dd41b
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-03-03 10:22:52 +0100

    Added tijgercamouflage style to FMP demos. Added HTML style to waterbok demo. Changed Code for Waterbok Demo.

--- demos/FMP/waterbok.js
+++ demos/FMP/waterbok.js
@@ -1,5 +1,6 @@
 import * as THREE from "../libs/three.js-r132/build/three.module.js";
 
+let SIZE = {x:0,y:0,width:0,height:0};
 
 
 const scene = new THREE.Scene();
@@ -10,6 +11,7 @@ const cube = new THREE.Mesh(geometry, material);
 const camera = new THREE.PerspectiveCamera();
 const renderer = new THREE.WebGLRenderer({alpha:true});
 
+/*
 const angles = ["alphaBetaGamma","alphaGammaBeta","betaAlphaGamma", "betaGammaAlhpa","gammaAlphaBeta","gammaBetaAlpha"];
 var currentangle = 0;
 
@@ -22,57 +24,69 @@ button.onclick = function(){
 		}
 	button.textContent = angles[currentangle];
 }
+*/
 
 window.addEventListener("deviceorientation", handleOrientation, true);
 document.addEventListener("DOMContentLoaded",()=>{
-	
+	const VIDEO = document.getElementById("VIDEO");
+	let promise = navigator.mediaDevices.getUserMedia({video: true, audio: false, video:{facingMode:"environment"}});
+	promise.then(function(signal) {
+		VIDEO.setAttribute('autoplay', '');
+		VIDEO.setAttribute('muted', '');
+		VIDEO.setAttribute('playsinline', '');
+		VIDEO.srcObject=signal;
+		VIDEO.play();
+		
+	}).catch(function(err) {
+		alert("Website werkt niet zonder cameratoestemming.");
+	});
+
+	VIDEO.addEventListener("canplay", function(e){
+			let resizer= window.innerWidth/VIDEO.videoWidth;
+
+			SIZE.width=resizer*VIDEO.videoWidth;
+			SIZE.height=resizer*VIDEO.videoHeight;
+			SIZE.x=VIDEO.width/2-SIZE.width/2;
+			SIZE.y=VIDEO.height/2-SIZE.height/2;
+
+			VIDEO.setAttribute("width", SIZE.width);
+			VIDEO.setAttribute("height",SIZE.height);
+	})
+
 	cube.position.set(0,0,-2);
 	cube.rotation.set(0,Math.PI/4,0);
 	scene.add(cube);
 
 	camera.position.set(1,1,5);
 	
-	renderer.setSize(500,500);
+	renderer.setSize(SIZE.width,SIZE.height);
 	renderer.render(scene,camera);
-	
-	const video = document.createElement("video");
-	navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
-		video.srcObject = stream;
-		video.play();
-	});
-	
-	video.style.position = "absolute";
-	video.style.width = renderer.domElement.width;
-	video.style.height = renderer.domElement.height;
-	renderer.domElement.style.position="absolute"
-	
-	document.body.appendChild(video);
-	document.body.appendChild(renderer.domElement);
 });
 
 function handleOrientation(event) {
-  var alpha    = event.alpha*(Math.PI/180);
-  var beta     = event.beta*(Math.PI/180);
-  var gamma    = event.gamma*(Math.PI/180);
-
-	  if (currentangle == 0){
-		camera.rotation.set(alpha,beta,gamma);
-	  }
-	  if (currentangle == 1){
-		camera.rotation.set(alpha,gamma,beta);
-	  }
-	  if (currentangle == 2){
-		camera.rotation.set(beta,alpha,gamma);
-	  }
-	  if (currentangle == 3){
-		camera.rotation.set(beta,gamma,alpha);
-	  }
-	  if (currentangle == 4){
-		camera.rotation.set(gamma,alpha,beta);
-	  }
-	  if (currentangle == 5){
-		camera.rotation.set(gamma,beta,alpha);
-	  }
-
-  renderer.render(scene,camera);
+	  var alpha    = event.alpha*(Math.PI/180);
+	  var beta     = event.beta*(Math.PI/180);
+	  var gamma    = event.gamma*(Math.PI/180);
+		/*
+		  if (currentangle == 0){
+			camera.rotation.set(alpha,beta,gamma);
+		  }
+		  if (currentangle == 1){
+			camera.rotation.set(alpha,gamma,beta);
+		  }
+		  if (currentangle == 2){
+			camera.rotation.set(beta,alpha,gamma);
+		  }
+		  if (currentangle == 3){
+			camera.rotation.set(beta,gamma,alpha);
+		  }
+		  if (currentangle == 4){
+			camera.rotation.set(gamma,alpha,beta);
+		  }
+		  if (currentangle == 5){
+			camera.rotation.set(gamma,beta,alpha);
+		  }
+		*/
+	camera.rotation.set(beta,gamma,alpha);
+	renderer.render(scene,camera);
 }
