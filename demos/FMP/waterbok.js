45cd81514ddaf0d7c08d7826f5a6557cc6ffba19
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-03-04 16:14:10 +0100

    More testing angles waterbok

--- demos/FMP/waterbok.js
+++ demos/FMP/waterbok.js
@@ -6,8 +6,20 @@ let SIZE = {x:0,y:0,width:0,height:0};
 const scene = new THREE.Scene();
 	
 const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: "#FF00FF"});
-const cube = new THREE.Mesh(geometry, material);
+const material1 = new THREE.MeshBasicMaterial({color: "#FF0000"});
+const material2 = new THREE.MeshBasicMaterial({color: "#00FF00"});
+const material3 = new THREE.MeshBasicMaterial({color: "#0000FF"});
+const material4 = new THREE.MeshBasicMaterial({color: "#FFFF00"});
+const material5 = new THREE.MeshBasicMaterial({color: "#00FFFF"});
+const material6 = new THREE.MeshBasicMaterial({color: "#FF00FF"});
+
+const cube1 = new THREE.Mesh(geometry, material1);
+const cube2 = new THREE.Mesh(geometry, material2);
+const cube3 = new THREE.Mesh(geometry, material3);
+const cube4 = new THREE.Mesh(geometry, material4);
+const cube5 = new THREE.Mesh(geometry, material5);
+const cube6 = new THREE.Mesh(geometry, material6);
+
 const camera = new THREE.PerspectiveCamera();
 const renderer = new THREE.WebGLRenderer({alpha:true});
 
@@ -59,11 +71,21 @@ document.addEventListener("DOMContentLoaded",()=>{
 			document.getElementById("spacer").style.height = String(SIZE.height).concat("px");
 	})
 
-	cube.position.set(1,0,0);
-	cube.rotation.set(0,Math.PI/4,0);
-	scene.add(cube);
+	cube1.position.set(3,0,0);
+	cube2.position.set(0,3,0);
+	cube3.position.set(0,0,3);
+	cube4.position.set(-3,0,0);
+	cube5.position.set(0,-3,0);
+	cube6.position.set(0,0,-3);
 
-	camera.position.set(0,0,5);
+	scene.add(cube1);
+	scene.add(cube2);
+	scene.add(cube3);
+	scene.add(cube4);
+	scene.add(cube5);
+	scene.add(cube6);
+
+	camera.position.set(0,0,0);
 	
 	renderer.render(scene,camera);
 
@@ -76,7 +98,11 @@ document.addEventListener("DOMContentLoaded",()=>{
 function handleOrientation(event) {
 	  var alpha    = event.alpha*(Math.PI/180);
 	  var beta     = event.beta*(Math.PI/180);
-	  var gamma    = event.gamma*(Math.PI/180);
+	  var gamma    = event.gamma*(Math.PI/90);
+
+	  document.getElementById("alphatext").innerHTML = "alphatext: ".concat(String(alpha));
+	  document.getElementById("betatext").innerHTML = "betatext: ".concat(String(beta));
+	  document.getElementById("gammatext").innerHTML = "gammatext: ".concat(String(gamma));
 		
 		  if (currentangle == 0){
 			camera.rotation.set(alpha,beta,gamma);
