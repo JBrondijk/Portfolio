b32ef34dcb43fc6e8b545ae5cdd507253ce3a6ee
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-05-10 19:03:42 +0200

    fes

--- demos/FMP/souvenirs.js
+++ demos/FMP/souvenirs.js
@@ -146,7 +146,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 		anchor.group.add(conveyor); //Build scene here.
 		scene.add(xrayPlane);
 		xrayPlane.position.set(0,-2,0);
-		xrayPlane.frustrumCulled = false;
+		xrayPlane.frustumCulled = false;
 		console.log(xrayPlane);
 
 		ARAnchor = anchor;
