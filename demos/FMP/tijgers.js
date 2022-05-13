1036fac7380c493d8a7235071fba5ccdb87fa421
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-05-13 20:27:42 +0200

    changed all font sizes from pt to rem tigers should be fixed to a "good enough" point now.

--- demos/FMP/tijgers.js
+++ demos/FMP/tijgers.js
@@ -18,7 +18,7 @@ let promise = navigator.mediaDevices.getUserMedia({video: true, audio: false, vi
 		VIDEO.play();
 
 		VIDEO.onloadeddata=function(){
-			if(ios()){
+			if(iOS()){
 				VIDEO.style.width = "0.0001px";	
 				VIDEO.style.height = "0.0001px";	
 				handleResize();
