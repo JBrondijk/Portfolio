63acb5d8cf170e6ce5ee2c17e70710c38831e5b2
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-05-13 17:03:05 +0200

    Finished TigerCamouflage Rework Added a style for the button container to the css document and added this style to all current menus.

--- demos/FMP/tijgers.js
+++ demos/FMP/tijgers.js
@@ -9,6 +9,8 @@ let CONTEXT = CANVAS.getContext("2d");
 CANVAS.width=CANVAS.clientWidth;
 CANVAS.height= CANVAS.clientHeight;
 
+var applyFilter = false;
+
 let promise = navigator.mediaDevices.getUserMedia({video: true, audio: false, video:{facingMode:"environment"}});
 	promise.then(function(signal) {
 		VIDEO.setAttribute('autoplay', '');
@@ -57,33 +59,50 @@ function handleResize(){
 function updateCanvas(){
 	CONTEXT.drawImage(VIDEO,SIZE.x,SIZE.y,SIZE.width,SIZE.height);
 	
-	var imageData = CONTEXT.getImageData(0,0,CANVAS.width,CANVAS.height);
-	var data = imageData.data;
+	
 	
 	//apply colorblindness filter
-	for(var id = 0, length = data.length; id < length; id += 4) {
-		var sr = data[id], // source-pixel
-			sg = data[id + 1],
-			sb = data[id + 2],
-			dr = sr, // destination-pixel
-			dg = sg,
-			db = sb;
+	if (applyFilter){
+		var imageData = CONTEXT.getImageData(0,0,CANVAS.width,CANVAS.height);
+		var data = imageData.data;
+
+		for(var id = 0, length = data.length; id < length; id += 4) {
+			var sr = data[id], // source-pixel
+				sg = data[id + 1],
+				sb = data[id + 2],
+				dr = sr, // destination-pixel
+				dg = sg,
+				db = sb;
 		
-			const m = [0.567,0.433,0,0,0, 0.558,0.442,0,0,0, 0,0.242,0.758,0,0, 0,0,0,1,0, 0,0,0,0,1]
-			dr=((sr*m[0])+(sg*m[1])+(sb*m[2])+(1*m[3])+m[4]);
-			dg=((sr*m[5])+(sg*m[6])+(sb*m[7])+(1*m[8])+m[9]);
-			db=((sr*m[10])+(sg*m[11])+(sb*m[12])+(1*m[13])+m[14]);
-			dr = fu(dr);
-			dg = fu(dg);
-			db = fu(db);
+				const m = [0.567,0.433,0,0,0, 0.558,0.442,0,0,0, 0,0.242,0.758,0,0, 0,0,0,1,0, 0,0,0,0,1]
+				dr=((sr*m[0])+(sg*m[1])+(sb*m[2])+(1*m[3])+m[4]);
+				dg=((sr*m[5])+(sg*m[6])+(sb*m[7])+(1*m[8])+m[9]);
+				db=((sr*m[10])+(sg*m[11])+(sb*m[12])+(1*m[13])+m[14]);
+				dr = fu(dr);
+				dg = fu(dg);
+				db = fu(db);
 				
-			data[id] = dr >> 0;
-			data[id + 1] = dg >> 0;
-			data[id + 2] = db >> 0;
-	}	
-	CONTEXT.putImageData(imageData,0,0);
-	
+				data[id] = dr >> 0;
+				data[id + 1] = dg >> 0;
+				data[id + 2] = db >> 0;
+		}	
+
+		CONTEXT.putImageData(imageData,0,0);
+	}
 	window.requestAnimationFrame(updateCanvas);
 }
 
 function fu(n){ return(n<0?0:(n<255?n:255)); }
+
+document.getElementById("btnStart").onclick = function(){
+	startMenu.style.display = "none";
+	document.getElementById("infoMenu").style.display = "block";
+	VIDEO.style.filter = "url(#protanopia)";
+	VIDEO.style.webkitFilter = "url(#protanopia)";
+	applyFilter = true;
+}
+
+document.getElementById("infoMenuHead").onclick = function(){
+	document.getElementById("infoMenu").classList.toggle("active");
+	document.getElementById("arrow").classList.toggle("active");
+}
