490399ff3bcadd5bd69f884e82dc9e6dffb274a7
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-05-16 14:50:46 +0200

    was making the selectplanes with texture instead of materials.

--- demos/FMP/boeren.js
+++ demos/FMP/boeren.js
@@ -44,19 +44,19 @@ const loader = new THREE.TextureLoader();
 const geometry = new THREE.PlaneGeometry(0.19,0.19);
 const textureNormal = loader.load("./textures/selectCircle.png");
 const textureHover = loader.load("./textures/selectCircleHover.png");
-const material = new THREE.MeshBasicMaterial({map: textureNormal, transparent:true, side:2,alphaTest: 0.1});
+const materialNormal = new THREE.MeshBasicMaterial({map: textureNormal, transparent:true, side:2,alphaTest: 0.1});
 const materialHover = new THREE.MeshBasicMaterial({map: textureHover, transparent:true, side:2,alphaTest: 0.1});
 
-const selectPlane1 = new THREE.Mesh(geometry, texture);
+const selectPlane1 = new THREE.Mesh(geometry, materialNormal);
 selectPlane1.position.set(-0.4,0.3,0);
 selectableObjects[0]=selectPlane1;
-const selectPlane2 = new THREE.Mesh(geometry, texture);
+const selectPlane2 = new THREE.Mesh(geometry, materialNormal);
 selectPlane2.position.set(-0.2,0.3,0);
 selectableObjects[1]=selectPlane2;
-const selectPlane3 = new THREE.Mesh(geometry, texture);
+const selectPlane3 = new THREE.Mesh(geometry, materialNormal);
 selectPlane3.position.set(0,0.3,0);
 selectableObjects[2]=selectPlane3;
-const selectPlane4 =  new THREE.Mesh(geometry, texture);
+const selectPlane4 =  new THREE.Mesh(geometry, materialNormal);
 selectPlane4.position.set(0.2,0.3,0);
 selectableObjects[3]=selectPlane4;
 
@@ -127,7 +127,7 @@ function loop (){
 
 	hoveredObject = findSelectedObject();
 	if (hoveredObject != previousHoveredObject){
-		previousHoveredObject.material = material();
+		previousHoveredObject.material = materialNormal();
 		if (hoveredObject != null){
 			hoveredObject.material = materialHover();
 			selectbtn.disabled = false;
