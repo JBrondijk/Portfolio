8fc0f9a75680f92b1f502872db88ea9d59eff7d0
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-05-27 23:22:15 +0200

    woops

--- demos/FMP/waterbok.js
+++ demos/FMP/waterbok.js
@@ -249,7 +249,7 @@ function updateProgress (){
 }
 
 function getIconPosition (iconPosition, icon){
-	return ((iconPosition/100)*95-(icon.clientWidth/document.body.clientWidth);
+	return ((iconPosition/100)*95-(icon.clientWidth/document.body.clientWidth));
 }
 function moveLion(){
 	lionDistance = lionDistance+0.7*delta;
