342f877e9b67ae6e93022bb057da07a438c18371
JurrienBrondijk <brondijkjurrien@gmail.com>
2022-06-01 00:11:39 +0200

    Generate Rhinos and add them to the enclosures.

--- demos/FMP/neushoorn.js
+++ demos/FMP/neushoorn.js
@@ -18,24 +18,127 @@ var gameState = "start",
 const	scanner = document.getElementById("scanning"),
 		startMenu = document.getElementById("startMenu"),
 		enclMenu = document.getElementById("enclMenu"),
-		enclInfo = document.getElementById("enclInfo");
+		enclInfo = document.getElementById("enclInfo"),
+		dummyrhino = document.getElementById("iconMaster");
 
-const infoMenus = [];
-	infoMenus[0] = document.getElementById ("encl0");
-	infoMenus[1] = document.getElementById ("encl1");
-	infoMenus[2] = document.getElementById ("encl2");
-	infoMenus[3] = document.getElementById ("encl3");
-	infoMenus[4] = document.getElementById ("encl4");
 
-const enclosureMenus = [];
-enclosureMenus[0] = document.getElementById("encl0Menu");
-enclosureMenus[1] = document.getElementById("encl1Menu");
-enclosureMenus[2] = document.getElementById("encl2Menu");
-enclosureMenus[3] = document.getElementById("encl3Menu");
-enclosureMenus[4] = document.getElementById("encl4Menu");
+//MAKING ALL THE ENCLOSURES/RHINOS
+const enclosures = [];
+	enclosures[0] = new enclosure(
+							document.getElementById("encl0"), 
+							document.getElementById("encl0Menu"), 
+							document.getElementById("encl0Males"), 
+							document.getElementById("encl0Females"), 
+							document.getElementById("encl0ProblemCount"), 
+							document.getElementById("menu0Fight"), 
+							document.getElementById("menu0Breed"), 
+							document.getElementById("menu0Man"), 
+							document.getElementById("menu0Done"), 
+							document.getElementById("encl0Contents"), 
+							document.getElementById("encl0Fight"), 
+							document.getElementById("encl0Breed"), 
+							document.getElementById("encl0Man"), 
+							document.getElementById("encl0Done"),
+							[new rhino (true, "#308DFF", "#308DFF"),
+							new rhino (false, "#308DFF", "#308DFF"),
+							new rhino (false, "# FF3030", "#FF30FF"),
+							new rhino (false, "#3030FF", "#3030FF")
+							]);
+	enclosures[1] = new enclosure(
+							document.getElementById("encl1"), 
+							document.getElementById("encl1Menu"), 
+							document.getElementById("encl1Males"), 
+							document.getElementById("encl1Females"), 
+							document.getElementById("encl1ProblemCount"), 
+							document.getElementById("menu1Fight"), 
+							document.getElementById("menu1Breed"), 
+							document.getElementById("menu1Man"), 
+							document.getElementById("menu1Done"), 
+							document.getElementById("encl1Contents"), 
+							document.getElementById("encl1Fight"), 
+							document.getElementById("encl1Breed"), 
+							document.getElementById("encl1Man"), 
+							document.getElementById("encl1Done"),
+							[new rhino (true, "#3030FF", "#3030FF"),
+							new rhino (true, "#308DFF", "#9030FF"),
+							new rhino (false, "#FF3030", "#3030FF"),
+							new rhino (false, "#FF3030", "#308DFF")
+							]);
+	enclosures[2] = new enclosure(
+							document.getElementById("encl2"), 
+							document.getElementById("encl2Menu"), 
+							document.getElementById("encl2Males"), 
+							document.getElementById("encl2Females"), 
+							document.getElementById("encl2ProblemCount"), 
+							document.getElementById("menu2Fight"), 
+							document.getElementById("menu0Breed"), 
+							document.getElementById("menu2Man"), 
+							document.getElementById("menu2Done"), 
+							document.getElementById("encl2Contents"), 
+							document.getElementById("encl2Fight"), 
+							document.getElementById("encl2Breed"), 
+							document.getElementById("encl2Man"), 
+							document.getElementById("encl2Done"),
+							[new rhino (true, "#FF3030", "#FF3030"),
+							new rhino (false, "#FFED30", "#FFED30"),
+							new rhino (false, "#9030FF", "#9030FF")
+							]);
+	enclosures[3] = new enclosure(
+							document.getElementById("encl3"), 
+							document.getElementById("encl3Menu"), 
+							document.getElementById("encl3Males"), 
+							document.getElementById("encl3Females"), 
+							document.getElementById("encl3ProblemCount"), 
+							document.getElementById("menu3Fight"), 
+							document.getElementById("menu3Breed"), 
+							document.getElementById("menu3Man"), 
+							document.getElementById("menu3Done"), 
+							document.getElementById("encl3Contents"), 
+							document.getElementById("encl3Fight"), 
+							document.getElementById("encl3Breed"), 
+							document.getElementById("encl3Man"), 
+							document.getElementById("encl3Done"), 
+							[new rhino (true, "#30FF41", "#30FF41"),
+							new rhino (true, "#FFED30", "#30FF41"),
+							new rhino (false, "#FF9030", "#FF9030"),
+							new rhino (false, "#30FFED", "#30FFED")
+							]);
+	enclosures[4] = new enclosure(
+							document.getElementById("encl4"), 
+							document.getElementById("encl4Menu"), 
+							document.getElementById("encl4Males"), 
+							document.getElementById("encl4Females"), 
+							document.getElementById("encl4ProblemCount"), 
+							document.getElementById("menu4Fight"), 
+							document.getElementById("menu4Breed"), 
+							document.getElementById("menu4Man"), 
+							document.getElementById("menu4Done"), 
+							document.getElementById("encl4Contents"), 
+							document.getElementById("encl4Fight"), 
+							document.getElementById("encl4Breed"), 
+							document.getElementById("encl4Man"), 
+							document.getElementById("encl4Done"), 
+							[new rhino (true, "#FF3030", "#FF3030"),
+							new rhino (true, "#FF3030", "#FF9030"),
+							new rhino (false, "#FF30FF", "#FF30FF"),
+							new rhino (false, "#FF9030", "#FF9030"),
+							new rhino (false, "#FF9030", "#FF3030")
+							]);
 
-//MAKING ALL THE RHINOS
-document.getElementById("encl0Males")
+//Setting all the border colors + appending elements
+for (var i = 0; i < enclosures.length; i++) {
+	for (var p = 0; p < enclosures[i].rhinos.length; p++) {
+		enclosures[i].rhinos[p].borderTopColor = enclosures[i].rhinos[p].gene1;
+		enclosures[i].rhinos[p].borderLeftColor = enclosures[i].rhinos[p].gene1;
+		enclosures[i].rhinos[p].borderRightColor = enclosures[i].rhinos[p].gene2;
+		enclosures[i].rhinos[p].borderBottomColor = enclosures[i].rhinos[p].gene2;
+		if (enclosures[i].rhinos[p].male){
+			enclosures[i].males.appendChild(enclosures[i].rhinos[p].div);
+		} else {
+			enclosures[i].females.appendChild(enclosures[i].rhinos[p].div);
+		}
+	}
+}
 
 //game variables
 var	openMenu;
@@ -124,12 +227,12 @@ function loop (){
 }
 
 function updateInfoMenuLocations (){
-	for (var i = 0; i < infoMenus.length; i++) {
+	for (var i = 0; i < enclosures.length; i++) {
 		let coords = getScreenLocation(enclosureTrackers[i])
-		coords.y= coords.y-(infoMenus[i].offsetWidth/2);
-		coords.x=coords.x- (infoMenus[i].offsetHeight/2);
-		infoMenus[i].style.top = coords.y+"px";
-		infoMenus[i].style.left= coords.x+"px";
+		coords.y= coords.y-(enclosures[i].infoMenu.offsetWidth/2);
+		coords.x=coords.x- (enclosures[i].infoMenu.offsetHeight/2);
+		enclosures[i].infoMenu.style.top = coords.y+"px";
+		enclosures[i].infoMenu.style.left= coords.x+"px";
 	}
 }
 
@@ -176,18 +279,33 @@ function distance2D(pointA, pointB){
 	return(Math.sqrt(distanceX*distanceX + distanceY*distanceY));
 }
 
-function rhino(div, male, gene1, gene2){
-	this.div = div;
+function rhino(male, gene1, gene2){
+	this.div = dummyrhino.cloneNode(true);
 	this.male = male;
 	this.gene1 = gene1;
 	this.gene2 = gene2;
 }
 
-function enclosure(rhinos, males, females, inbreeding){
-	this.rhinos = rhinos;
+function enclosure(infoMenu, enclosureMenu, males, females, problemcount, menuFight, menuBreed, menuMan, menuDone, infoContents, infoFight, infoBreed, infoMan, infoDone, rhinos){
+	this.infoMenu = infoMenu;
+	this.enclosureMenu = enclosureMenu;
 	this.males = males;
 	this.females = females;
-	this.inbreeding = inbreeding;
+	this.problemcount = problemcount;
+	this.menuFight = menuFight;
+	this.menuBreed = menuBreed;
+	this.menuMan = menuMan;
+	this.menuDone = menuDone;
+	this.infoContents = infoContents;
+	this.infoFight = infoFight;
+	this.infoBreed = infoBreed;
+	this.infoMan = infoMan;
+	this.infoDone = infoDone;
+	this.rhinos = rhinos;
+
+	this.maleCount = 0;
+	this.femaleCount = 0;
+	this.inbreeding = false;
 }
 
 document.getElementById("btnStart").onclick = function(){
@@ -204,10 +322,10 @@ function openEnclosureMenu(menuToOpen){
 	if (gameState == "play"){
 		//set the correct menu to open
 		if (openMenu!= null){
-			enclosureMenus[openMenu].style.display = "none";
+			enclosures[openMenu].enclosureMenu.style.display = "none";
 		}
 		openMenu = menuToOpen;
-		enclosureMenus[openMenu].style.display = "block";
+		enclosures[openMenu].enclosureMenu.style.display = "block";
 		gameState = "menu";
 		updateUI();
 	}
